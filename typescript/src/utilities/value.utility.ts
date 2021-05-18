bb77988a8f915af5e0ed402062c8b381ae96b15f
eric <eric.christensen94@gmail.com>
2021-05-18 22:52:36 +0200

    And another

--- typescript/src/utilities/value.utility.ts
+++
@@ -1,8 +0,0 @@
-
-export class ValueUtility {
-    static readonly EMAIL_REGEX = /^[^\s@]+@[^\s@]+$/;
-
-    static isEmail(value: string): boolean {
-        return ValueUtility.EMAIL_REGEX.test(value);
-    }
-}
