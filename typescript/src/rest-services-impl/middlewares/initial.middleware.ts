9d0d076f30be7602eb5e2a88440331a9c76c4e7b
fyllekanin <eric.christensen94@gmail.com>
2021-05-19 13:30:21 +0200

    Update initial.middleware.ts

--- typescript/src/rest-services-impl/middlewares/initial.middleware.ts
+++ typescript/src/rest-services-impl/middlewares/initial.middleware.ts
@@ -2,6 +2,8 @@ import { NextFunction, Response } from 'express';
 import { InternalRequest } from '../../utilities/internal.request';
 import { verify } from 'jsonwebtoken';
 
+const TOKEN_SECRET = 'a95d8e70bf0b967df0c7e2a58981c388d9e19de834ff536097130ece404e86351ddd946a048fa0276ce6f0054e417ed67a87e053599d0b5942662ba6a1231f66';
+
 export const INITIAL_MIDDLEWARE = async (req: InternalRequest, res: Response, next: NextFunction) => {
     const authHeader = req.header('authorization');
     const token = authHeader ? authHeader.split(' ')[1] : null;
@@ -14,7 +16,7 @@ export const INITIAL_MIDDLEWARE = async (req: InternalRequest, res: Response, ne
         next();
         return;
     }
-    const user = verify(token, process.env.TOKEN_SECRET) as { _id: string };
+    const user = verify(token, TOKEN_SECRET) as { _id: string };
 
     if (!user) {
         req.user = {
