2dd03c71673c5aee8c2083c1f96837ce1d42a4b6
Eric <eric.christensen94@gmail.com>
2022-04-03 10:45:27 +0200

    Fix syntax

--- react/todo-list/src/components/add-task/AddTask.js
+++ react/todo-list/src/components/add-task/AddTask.js
@@ -2,7 +2,7 @@ import './AddTask.css';
 import { useState } from "react";
 
 function AddTask(props) {
-    const [state, setState] = useState({value: '', isError: false});
+    const [state, setState] = useState({ value: '', isError: false });
     const error = state.isError ? <div className="error">The task {state.value} already exists</div> : '';
 
     const onAdd = () => {
@@ -10,14 +10,14 @@ function AddTask(props) {
             return;
         }
         props.onAdd(state.value);
-        setState({value: '', isError: false});
+        setState({ value: '', isError: false });
     };
 
     return (
         <div className="AddTask">
             {error}
             <input type="text" placeholder="Add new task..." id="add-task-input" value={state.value}
-                   onChange={evt => setState({value: evt.target.value, isError: state.isError})}/>
+                   onChange={evt => setState({ value: evt.target.value, isError: state.isError })}/>
             <button onClick={onAdd}>Add</button>
         </div>
     )
--- react/todo-list/src/components/todo-task/TodoTask.js
+++ react/todo-list/src/components/todo-task/TodoTask.js
@@ -2,17 +2,17 @@ import './TodoTask.css';
 import { useState } from "react";
 
 function TodoTask(props) {
-    const [state, setState] = useState({isInitial: true});
+    const [state, setState] = useState({ isInitial: true });
     if (state.isInitial) {
         setTimeout(() => {
-            setState({isInitial: false});
+            setState({ isInitial: false });
         }, 200);
     }
 
     const doneButton = props.task.isComplete ? null :
         <button className="done" onClick={() => props.onComplete(props.task.name)}>Done</button>;
     const discardClasses = props.task.isComplete ? 'discard full-button' : 'discard';
-    const mainClasses = `TodoTask${state.isInitial ? ' hidden' : ''}`
+    const mainClasses = `TodoTask${state.isInitial ? ' hidden' : ''}`;
 
     return (
         <div className={mainClasses}>
